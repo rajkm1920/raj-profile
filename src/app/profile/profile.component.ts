7dd624d3a213464ecd72a21933b3ff0c52480d6c
Raj Kumar <rajkumar@spadeinfotech.net>
2022-01-24 07:44:02 +0530

    update-exp

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -8,36 +7,32 @@ import { DatePipe } from '@angular/common';
 })
 export class ProfileComponent implements OnInit {
   today: Date = new Date();
-  exper: string = '2 years 10 months';
-  startDate :Date  = new Date(2016, 8, 1);
 
+  //8= Sep start index 0=Jan
+  startDate: Date = new Date(2020, 8, 1);
+  start_mont: number = (this.startDate.getMonth() + 1);
+  start_year: number = (this.startDate.getFullYear());
 
-  start_mont:number= (this.startDate.getMonth()+1);
- start_year:number= (this.startDate.getFullYear());
+  current_mont: number = (this.today.getMonth() + 1);
+  current_year: number = (this.today.getFullYear());
+  completedate: string = "";
 
-current_mont:number= (this.today.getMonth()+1);
-current_year:number= (this.today.getFullYear());
-completedate:string="";
+  year: number;
+  month: number;
 
-  year: number ;
-  month:number ;
-  
   constructor() { }
   ngOnInit() {
-    if(this.start_mont>this.current_mont){
-      this.year=(this.current_year-this.start_year-1);
-      this.month=(12-this.start_mont)+this.current_mont;
-      this.completedate=this.year +" year "+ this.month +" month";
-      }else{
-        this.year=(this.current_year-this.start_year);
-      if(this.start_mont==this.current_mont){
-        this.completedate=this.year+" year";
-      }else{   
-      this.completedate=this.year +" year "+(this.current_mont-this.start_mont)+" month ";
+    if (this.start_mont > this.current_mont) {
+      this.year = (this.current_year - this.start_year - 1);
+      this.month = (12 - this.start_mont) + this.current_mont;
+      this.completedate = this.year + " year " + this.month + " month";
+    } else {
+      this.year = (this.current_year - this.start_year);
+      if (this.start_mont == this.current_mont) {
+        this.completedate = this.year + " year";
+      } else {
+        this.completedate = this.year + " year " + (this.current_mont - this.start_mont) + " month ";
       }
-      }
+    }
   }
-
-  
-
 }
